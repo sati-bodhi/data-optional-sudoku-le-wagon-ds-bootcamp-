e52e59d75147716da0b257600fe676f03646aff0
Tan Yin Hoe <astyh83@gmail.com>
2023-05-03 17:12:20 +0800

    linter

--- sudoku.py
+++ sudoku.py
@@ -5,8 +5,7 @@ def sudoku_validator(grid):
     for block in grid:
         if set(block) != {1,2,3,4,5,6,7,8,9}:
             return False
-    
-
+            
     for i in range(9):
         test = []
         for j in range(9):
@@ -28,20 +27,3 @@ def sudoku_validator(grid):
                 return False
 
     return True
-
-
-grid = [
-    [1,2,3, 4,5,6, 7,8,9],
-    [2,3,1, 5,6,4, 8,9,7],
-    [3,1,2, 6,4,5, 9,7,8],
-
-    [4,5,6, 7,8,9, 1,2,3],
-    [5,6,4, 8,9,7, 2,3,1],
-    [6,4,5, 9,7,8, 3,1,2],
-
-    [7,8,9, 1,2,3, 4,5,6],
-    [8,9,7, 2,3,1, 5,6,4],
-    [9,7,8, 3,1,2, 6,4,5]
-]
-
-sudoku_validator(grid)
